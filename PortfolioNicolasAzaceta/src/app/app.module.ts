36fee9b2fcf06eb8c20b908c5205a89466078fea
NicolasAzaceta <nicolasazaceta@gmail.com>
2023-03-31 20:11:01 +0200

    Proyectos

--- PortfolioNicolasAzaceta/src/app/app.module.ts
+++ PortfolioNicolasAzaceta/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HabilidadesComponent } from './components/habilidades/habilidades.compo
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HabilidadesComponent
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
