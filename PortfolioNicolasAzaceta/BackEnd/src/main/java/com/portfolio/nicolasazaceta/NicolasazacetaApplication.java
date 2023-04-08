39e246d1d94495cc01a4e2fd38413ce73fae50b3
NicolasAzaceta <nicolasazaceta@gmail.com>
2023-04-08 19:58:29 +0200

    Creación carpeta "BackEnd"

---
+++ PortfolioNicolasAzaceta/BackEnd/src/main/java/com/portfolio/nicolasazaceta/NicolasazacetaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nicolasazaceta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolasazacetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolasazacetaApplication.class, args);
+	}
+
+}
---
+++ PortfolioNicolasAzaceta/BackEnd/src/test/java/com/portfolio/nicolasazaceta/NicolasazacetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.nicolasazaceta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasazacetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
